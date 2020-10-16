3913702e959b7a2fc9d3f94dce6d4f05a75cc063
Robert Adams <misterblue@misterblue.com>
2020-10-16 07:37:25 -0700

    When invoking assignment-clients, specify the domain-server with the     host's IP address rather than "localhost".     Docker looks like it has oddities with "localhost" and UDP traffic.

--- files/start-domain-server.sh
+++ files/start-domain-server.sh
@@ -14,12 +14,14 @@ RUNDIR=/opt/vircadia/install_master
 
 cd "${RUNDIR}"
 
-./run_assignment-client -t 0 -a localhost -p 48000 >> "${ALOGFILE}-0.log" 2>&1 &
-./run_assignment-client -t 1 -a localhost -p 48001 >> "${ALOGFILE}-1.log" 2>&1 &
-./run_assignment-client -t 2 -a localhost -max 100 >> "${ALOGFILE}-2.log" 2>&1 &
-./run_assignment-client -t 3 -a localhost -p 48003 >> "${ALOGFILE}-3.log" 2>&1 &
-./run_assignment-client -t 4 -a localhost -p 48004 >> "${ALOGFILE}-4.log" 2>&1 &
-./run_assignment-client -t 5 -a localhost -p 48005 >> "${ALOGFILE}-5.log" 2>&1 &
-./run_assignment-client -t 6 -a localhost -p 48006 >> "${ALOGFILE}-6.log" 2>&1 &
+IP=$(hostname -I)
+
+./run_assignment-client -t 0 -a ${IP} -p 48000 >> "${ALOGFILE}-0.log" 2>&1 &
+./run_assignment-client -t 1 -a ${IP} -p 48001 >> "${ALOGFILE}-1.log" 2>&1 &
+./run_assignment-client -t 2 -a ${IP} -max 100 >> "${ALOGFILE}-2.log" 2>&1 &
+./run_assignment-client -t 3 -a ${IP} -p 48003 >> "${ALOGFILE}-3.log" 2>&1 &
+./run_assignment-client -t 4 -a ${IP} -p 48004 >> "${ALOGFILE}-4.log" 2>&1 &
+./run_assignment-client -t 5 -a ${IP} -p 48005 >> "${ALOGFILE}-5.log" 2>&1 &
+./run_assignment-client -t 6 -a ${IP} -p 48006 >> "${ALOGFILE}-6.log" 2>&1 &
 
 ./run_domain-server -i ${ICE_SERVER_URL} >> "${LOGFILE}.log" 2>&1
