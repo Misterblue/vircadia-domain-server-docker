06af0ec8726f0805ab6666a793c2504e507f174e
Robert Adams <misterblue@misterblue.com>
2021-02-23 10:34:19 -0800

    Add repository variable that allows setting the source repository to  use.

--- buildBase.sh
+++ buildBase.sh
@@ -3,10 +3,12 @@
 #    libraries (Qt, ...) and the Vircadia server apps (domain-server, ...).
 
 # Set the environment variable TAG to the github tag to pull.
+export BUILDREPO=${VIRCADIA_REPO:-https://github.com/vircadia/vircadia}
 export BUILDTAG=${VIRCADIA_TAG:-master}
 
-echo "=== Building $BUILDTAG"
+echo "=== Building ${BUILDREPO}:${BUILDTAG}"
 
 docker build -f Dockerfile-base \
+        --build-arg REPO=${BUILDREPO} \
         --build-arg TAG=${BUILDTAG} \
         -t domain-server-build-base . | tee ./out.buildBase 2>&1
--- buildDS.sh
+++ buildDS.sh
@@ -2,6 +2,8 @@
 # Build the domain-server Docker image based on the base image.
 # Invocation: ./buildDS.sh {GIT-tag-to-pull}
 
+export BUILDREPO=${VIRCADIA_REPO:-https://github.com/vircadia/vircadia}
+
 # if the env variable exists and no parameter specified, use that
 if [[ ! -z "$VIRCADIA_TAG" && -z "$1" ]] ; then
     ATAG=$VIRCADIA_TAG
@@ -14,7 +16,10 @@ fi
 # vircadia-builder normalizes the tag name with all special chars to underscore
 export BUILDTAG=$(echo $ATAG | sed -e 's/[[:punct:]]/_/g')
 
+echo "Building Vircadia domain server ${BUILDREPO}:${BUILDTAG}"
+
 docker build -f Dockerfile-ds \
+    --build-arg REPO=${BUILDREPO} \
     --build-arg TAG=${BUILDTAG} \
     ${NOCACHE} \
     -t vircadia-domain-server . | tee out.buildDS 2>&1
