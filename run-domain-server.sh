b55d405f5fa84274d448db6354fc7765b7725ba0
Robert Adams <misterblue@misterblue.com>
2021-10-31 23:26:58 +0000

    Add where DOCKER_REPOSITORY=LOCAL means to use the local images. Rename ENV_ variables to be their external names to reduce confusion.

--- run-domain-server.sh
+++ run-domain-server.sh
@@ -3,14 +3,33 @@
 # Invocation: ./run-domain-server {metaverse_server_url} {ice_server_location}
 #   or the metaverse-server and ice-server can be set with environment
 #      variables METAVERSE_URL and ICE_SERVER
+# This uses several environment variables for various options:
+#    METAVERSE_URL: URL to metaverse-server (default = https://metaverse.vircadia.com/live)
+#    ICE_SERVER: URL to the ICE server (default = "ice.vircadia.com:7337")
+#    DOCKER_REPOSITORY: name of repository to fetch image (default = "misterblue")
+#         if DOCKER_REPOSITORY === "LOCAL" then the local image repository is used
+#    IMAGE_NAME: name of docker image to run (default = "vircadia-domain-server")
+#    IMAGE_VERSION: version of docker image to use (default = "latest")
+# With no changes, this runs the Docker image "misterblue/vircadia-domain-server:latest"
+#    pointing to the metaverse-server at "https://metaverse.vircadia.com/live"
+#    and ICE server "ice.vircadia.com:7337"..
 
-export ENV_METAVERSE_URL=${METAVERSE_URL:-https://metaverse.vircadia.com/live}
+export METAVERSE_URL=${METAVERSE_URL:-https://metaverse.vircadia.com/live}
 if [[ ! -z "$1" ]] ; then
-    ENV_METAVERSE_URL=$1
+    METAVERSE_URL=$1
 fi
-export ENV_ICE_SERVER=${ICE_SERVER:-ice.vircadia.com:7337}
+export ICE_SERVER=${ICE_SERVER:-ice.vircadia.com:7337}
 if [[ ! -z "$2" ]] ; then
-    ENV_ICE_SERVER=$2
+    ICE_SERVER=$2
+fi
+
+export DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-misterblue}
+export IMAGE_NAME=${IMAGE_NAME:-vircadia-domain-server}
+export IMAGE_VERSION=${IMAGE_VERSION:-latest}
+if [[ "${DOCKER_REPOSITORY}" == "LOCAL" ]] ; then
+    export DOCKER_IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}
+else
+    export DOCKER_IMAGE=${DOCKER_REPOSITORY}/${IMAGE_NAME}:${IMAGE_VERSION}
 fi
 
 # There can be multiple domain-servers on one host. This is the default index
@@ -27,15 +46,14 @@ DOTLOCALDIR=${BASE}/server-dotlocal/${CLEANMVNAME}
 LOGDIR=${BASE}/server-logs/${CLEANMVNAME}/$INSTANCE
 
 # Debugging stuff that can be removed
-echo "METAVERSE_URL=${ENV_METAVERSE_URL}"
-echo "ICE_SERVER=${ENV_ICE_SERVER}"
+echo "METAVERSE_URL=${METAVERSE_URL}"
+echo "ICE_SERVER=${ICE_SERVER}"
 echo "CLEANMVNAME=${CLEANMVNAME}"
 echo "DOTLOCALDIR=${DOTLOCALDIR}"
 echo "LOGDIR=${LOGDIR}"
+echo "DOCKER IMAGE=${DOCKER_IMAGE}"
 
 cd "${BASE}"
-
-DVERSION=latest
 
 # Create local directories that are mounted from the Docker container
 # Permission is set to 777 since the container runs its own UID:GID
@@ -54,7 +72,7 @@ docker run \
         --network=host \
         --volume ${DOTLOCALDIR}:/home/cadia/.local \
         --volume ${LOGDIR}:/home/cadia/logs \
-        misterblue/vircadia-domain-server:${DVERSION}
+        ${DOCKER_IMAGE}
 
 # The assignment clients are all over the place so network=host works
 #        -p 40100-40102:40100-40102 -p 40100-40102:40100-40102/udp \
--- run-ice-server.sh
+++ run-ice-server.sh
@@ -2,17 +2,36 @@
 # Start the ice-server with persistant data in local dir
 # Invocation: ./run-ice-server {metaverse_server_url}
 #    or the metaverse url can be set with environment variable METAVERSE_URL
+# This uses several environment variables for various options:
+#    METAVERSE_URL: URL to metaverse-server (default = https://metaverse.vircadia.com/live)
+#    ICE_SERVER: URL to the ICE server (default = "ice.vircadia.com:7337")
+#    DOCKER_REPOSITORY: name of repository to fetch image (default = "misterblue")
+#         if DOCKER_REPOSITORY === "LOCAL" then the local image repository is used
+#    IMAGE_NAME: name of docker image to run (default = "vircadia-domain-server")
+#    IMAGE_VERSION: version of docker image to use (default = "latest")
+# With no changes, this runs the Docker image "misterblue/vircadia-domain-server:latest"
+#    pointing to the metaverse-server at "https://metaverse.vircadia.com/live"
+#    and run as ICE server "ice.vircadia.com:7337"..
 
-export ENV_METAVERSE_URL=${METAVERSE_URL:-https://metaverse.vircadia.com/live}
+export METAVERSE_URL=${METAVERSE_URL:-https://metaverse.vircadia.com/live}
 if [[ ! -z "$1" ]] ; then
-    ENV_METAVERSE_URL=$1
+    METAVERSE_URL=$1
+fi
+
+export DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-misterblue}
+export IMAGE_NAME=${IMAGE_NAME:-vircadia-domain-server}
+export IMAGE_VERSION=${IMAGE_VERSION:-latest}
+if [[ "${DOCKER_REPOSITORY}" == "LOCAL" ]] ; then
+    export DOCKER_IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}
+else
+    export DOCKER_IMAGE=${DOCKER_REPOSITORY}/${IMAGE_NAME}:${IMAGE_VERSION}
 fi
 
 # Get the directory that this script is running from
 BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
 
 # Create a cleaned up version of the METAVERSE_URL to use as directory for per-grid info
-CLEANMVNAME=$(echo $ENV_METAVERSE_URL | sed -e 's=http.*://==' -e 's/[[:punct:]]/./g')
+CLEANMVNAME=$(echo $METAVERSE_URL | sed -e 's=http.*://==' -e 's/[[:punct:]]/./g')
 
 DOTLOCALDIR=${BASE}/server-dotlocal/${CLEANMVNAME}
 LOGDIR=${BASE}/server-logs/${CLEANMVNAME}
@@ -20,10 +39,10 @@ LOGDIR=${BASE}/server-logs/${CLEANMVNAME}
 DVERSION=latest
 
 # Debugging stuff that can be removed
-echo "METAVERSE_URL=${ENV_METAVERSE_URL}"
+echo "METAVERSE_URL=${METAVERSE_URL}"
 echo "DOTLOCALDIR=${DOTLOCALDIR}"
 echo "LOGDIR=${LOGDIR}"
-echo "DVERSION=${DVERSION}"
+echo "DOCKER_IMAGE=${DOCKER_IMAGE}"
 
 cd "${BASE}"
 
@@ -40,7 +59,7 @@ docker run \
         --name=iceserver \
         -p 7337:7337 \
         -p 7337:7337/udp \
-        -e METAVERSE_URL=$ENV_METAVERSE_URL \
+        -e METAVERSE_URL=$METAVERSE_URL \
         --volume ${LOGDIR}:/home/cadia/logs \
         --entrypoint /home/cadia/start-ice-server.sh \
-        misterblue/vircadia-domain-server:${DVERSION}
+        ${DOCKER_IMAGE}
